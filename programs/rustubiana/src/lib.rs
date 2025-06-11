5317dae70e10701b5292c3439d333b6b19e22646
Pedro Martins <sarumankeeb@gmail.com>
2025-06-11 13:12:50 +0100

    Implements basic auction functionality.
    Implements core auction functionalities including creation, bidding, and ending auctions.
    It includes transferring the NFT to the auction account, handling bids, and transferring the NFT to the winner.
    
    Includes TODOs and FIXMEs for future improvements and known issues.

--- programs/rustubiana/src/lib.rs
+++ programs/rustubiana/src/lib.rs
@@ -1,26 +1,38 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::clock;
-use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
+use anchor_spl::token;
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 declare_id!("6jm6mnCoAMJe4ZbvoBXfeiNJ1Bb8kz29Yq2HsQBBzezQ");
 
 #[program]
 pub mod rustubiana {
-
     use super::*;
 
-    pub fn init_auction(
-        ctx: Context<InitAuction>,
-        _starting_bid: u64,
-        _duration: i64,
+    pub fn create_auction(
+        ctx: Context<CreateAuction>,
+        auction_id: u64,
+        min_bid: u64,
+        duration: i64,
     ) -> Result<()> {
         let auction = &mut ctx.accounts.auction;
-        auction.owner = *ctx.accounts.owner.key;
-        auction.nft = ctx.accounts.nft.key();
+        auction.authority = *ctx.accounts.authority.key;
+        auction.nft_mint = ctx.accounts.nft_mint.key();
         auction.highest_bid = 0;
-        auction.highest_bidder = Pubkey::default();
-        auction.end_time = Clock::get()?.unix_timestamp + _duration;
-        auction.state = true;
+        auction.highest_bidder = None;
+        auction.min_bid = min_bid;
+        auction.ended = false;
+        auction.end_time = Clock::get()?.unix_timestamp + duration;
+        auction.auction_id = auction_id;
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.seller_token_account.to_account_info(),
+            to: ctx.accounts.auction_token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+
+        token::transfer(cpi_ctx, 1)?;
 
         Ok(())
     }
@@ -63,36 +75,6 @@ pub mod rustubiana {
     }
 }
 
-#[derive(Accounts)]
-pub struct InitAuction<'info> {
-    #[account(init, payer = owner, space = 8 + 129)]
-    pub auction: Account<'info, Auction>,
-
-    pub nft: Account<'info, Mint>,
-
-    #[account(mut)]
-    pub owner: Signer<'info>,
-
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Bid<'info> {
-    #[account(mut, has_one = nft)]
-    pub auction: Account<'info, Auction>,
-
-    #[account(mut)]
-    pub bidder: Signer<'info>,
-
-    #[account(mut)]
-    pub pda_account: UncheckedAccount<'info>,
-
-    #[account(mut)]
-    pub previous_highest_bidder: UncheckedAccount<'info>,
-
-    pub system_program: Program<'info, System>,
-}
-
 // TODO:
 // Implementar um PDA Account
 //
@@ -103,42 +85,111 @@ pub struct Bid<'info> {
 // FIXME:
 // Arranjar a parte do NFT (i don't know anymore at this point)
 
+// Structs
+
 #[derive(Accounts)]
-pub struct EndAuction<'info> {
-    #[account(mut, has_one = owner, has_one = nft)]
+#[instruction(auction_id: u64)]
+pub struct CreateAuction<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Auction::LEN,
+        seeds = [b"auction".as_ref(), &auction_id.to_le_bytes()],
+        bump
+    )]
     pub auction: Account<'info, Auction>,
 
     #[account(mut)]
-    pub owner: Signer<'info>,
+    pub nft: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        constraint = seller_token_account.amount >= 1,
+        constraint = seller_token_account.mint == nft_mint.key()
+    )]
+    pub seller_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = authority,
+        token::mint = nft_mint,
+        token::authority = auction,
+        seeds = [b"auction_token_account".as_ref(), &auction_id.to_le_bytes()],
+        bump
+    )]
+    pub auction_token_account: Account<'info, TokenAccount>,
 
     #[account(mut)]
-    pub nft_seller: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub nft_buyer: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub pda_account: UncheckedAccount<'info>,
+    pub authority: Signer<'info>,
 
     pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
-    pub system_program: Program<'info, System>,
+#[derive(Accounts)]
+pub struct PlaceBid<'info> {
+    #[account(mut)]
+    pub auction: Account<'info, Auction>,
+
+    #[account(mut)]
+    pub bidder: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = bidder_token_account.owner == bidder.key(),
+    )]
+    pub bidder_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = auction_sol_account.owner == auction.key(),
+    )]
+    pub auction_sol_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct EndAuction<'info> {
+    #[account(mut, seeds = [b"auction".as_ref(), &auctino.auction_id.to_le_bytes()], bump)]
+    pub auction: Account<'info, Auction>,
+
+    #[account(mut)]
+    pub seller_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut, constraint = auction_token_account.mint == auction.key())]
+    pub auction_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut, constraint = winner_token_account.mint == auction.nft_mint)]
+    pub winner: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
 }
 
 #[account]
 pub struct Auction {
-    pub owner: Pubkey,
-    pub nft: Pubkey,
+    pub authority: Pubkey,
+    pub nft_mint: Pubkey,
     pub highest_bid: u64,
-    pub highest_bidder: Pubkey,
+    pub highest_bidder: Option<Pubkey>,
+    pub min_bid: u64,
+    pub ended: bool,
     pub end_time: i64,
-    pub state: bool,
+    pub auction_id: u64,
+}
+
+// Espaço pre-alocado para o contrato
+impl Auction {
+    pub const LEN: usize = 32 + 32 + 8 + 1 + 32 + 8 + 1 + 8 + 8;
 }
 
 #[error_code]
 pub enum ErrorCode {
     #[msg("The auction has already ended.")]
     AuctionAlreadyEnded,
+    #[msg("The auction still open.")]
+    AuctionNotEnded,
     #[msg("Your bid must be higher than the current highest bid.")]
     BidTooLow,
 }
